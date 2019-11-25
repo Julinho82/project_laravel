997e00a5c1750b48bc0616d52554df3feaa19288
Julio Daviú Arévalo <juliolotr@gmail.com>
2019-11-25 10:07:32 -0400

    Request modoPagos

--- app/Http/Requests/Api/ModoPagos/StoreRequest.php
+++ app/Http/Requests/Api/ModoPagos/StoreRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests\Api\ModoPagos;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class StoreRequest extends FormRequest
 {
@@ -25,6 +29,22 @@ class StoreRequest extends FormRequest
     {
         return [
             //
+            'nombre'=>'required',
+            'otros_detalles'=>'required',
+
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    { 
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'success'=>false,
+                'data'=>$errors,
+                'message'=> "Error al validar los campos de ModoPagos"
+            ],
+            JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+        ));
     }
 }
