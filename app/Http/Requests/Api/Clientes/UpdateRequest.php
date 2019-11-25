ffe7dc914f7cc01fe60404f91267697d6502aaf7
Julio Daviú Arévalo <juliolotr@gmail.com>
2019-11-25 03:22:16 -0400

    Completando updateRequest Cliente

--- app/Http/Requests/Api/Clientes/UpdateRequest.php
+++ app/Http/Requests/Api/Clientes/UpdateRequest.php
@@ -24,7 +24,24 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nombre' => 'required|min:5|max:20',
+            'apellido' => 'required',
+            'fecha_nacimiento' => 'required',
+            'telefono' => 'required',
+            'email' => 'required'
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    { 
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'success'=>false,
+                'data'=>$errors,
+                'message'=> "Error al validar los campos haciendo Update"
+            ],
+            JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+        ));
     }
 }
