b58ba2721ffc9a6681eec74b52d3871dce672146
Julio Daviú Arévalo <juliolotr@gmail.com>
2019-11-25 10:59:56 -0400

    Producto Controller

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -1,10 +1,43 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api; //Lugar de trabajo
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BaseController; // Updated
+use App\Models\Producto;
+use App\Http\Requests\Api\Productos\StoreRequest;
+use App\Http\Requests\Api\Productos\UpdateRequest;
 
-class ProductoController extends Controller
+class ProductoController extends BaseController
 {
-    //
+    //Listar todos los productos
+    public function index()
+    {
+        $productoList = Producto::all();
+        return $this->sendResponse($productoList, "LISTA DE PRODUCTOS RECUPERADA");
+    }
+    //Recuperar un producto por id
+    public function show(Producto $producto)
+    {
+        return $this->sendResponse($producto, "PRODUCTO RECUPERADO");
+    }
+    //Editar un producto por id
+    public function update(UpdateRequest $request, Producto $producto)
+    {
+        $producto->update($request->all());
+        return $this->sendResponse($producto, "PRODUCTO EDITADO");
+    }
+    //Eliminar un producto por id
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+        return $this->sendResponse($producto, "PRODUCTO ELIMINADO");
+    }
+
+    //Crear un producto
+    public function store(StoreRequest $request)
+    {
+        $producto = Producto::create($request->all());
+        return $this->sendResponse($producto, "PRODUCTO CREADO");
+    }
 }
